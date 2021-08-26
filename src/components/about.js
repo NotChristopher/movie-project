a4271bc082e5fae7419aa36480a891d234576087
NotChristopher <64840908+NotChristopher@users.noreply.github.com>
2021-08-26 17:10:22 +0200

    added page github link to github link in about

--- src/components/about.js
+++ src/components/about.js
@@ -10,7 +10,7 @@ const About = () => {
             <p className="lead">
              This is a porfolio project to showcase my frontend and react.js developer skills. This app shows the details of searched movies. Details includes, overview, rating and runtime of a selected movie.
              The content comes from tmdb.org api and is formated using css and bootstrap component to make it not only look and feel nice to use but to make it responsive on all devices. 
-             The Home page displays trending movies for the current week. Visit my <a href = "githublink">Github</a> to view the source code, make sure to read the "Readme.md" file to help you navigate the code and for 
+             The Home page displays trending movies for the current week. Visit my <a href = "https://github.com/NotChristopher/movie-project">Github</a> to view the source code, make sure to read the "Readme.md" file to help you navigate the code and for 
              a look on how I went about creating this app. 
              Thank you for your time.
             </p>
